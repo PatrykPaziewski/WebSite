1982fa6778036ff099fb2fe34313b76852e4e457
Patryk Paziewski <p.paziewski@icloud.com>
2019-08-09 00:30:49 +0200

    SignIn/Login page with LWT authorization and AuthGuard

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(AuthenticationContext context)
+        {
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
